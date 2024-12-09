a03b5f57a0b6f9f4345e5add1fd496acff9b77f1
mnbd <mnbdpro@gmail.com>
2024-12-09 15:16:43 +0100

    TP 1

---
+++ Projects/mvcTemplate/Controllers/TeacherController.cs
@@ -0,0 +1,19 @@
+using Microsoft.AspNetCore.Mvc;
+
+using mvc.Models;
+
+public class TeacherController : Controller
+{
+    // liste d'enseignants
+    private static List<Teacher> _teachers = new List<Teacher>
+    {
+        new Teacher { Id = 1, Lastname = "Doe", Firstname = "John" },
+        new Teacher { Id = 2, Lastname = "Smith", Firstname = "Jane" }
+    };
+
+
+    public IActionResult Index()
+    {
+        return View(_teachers);
+    }
+}
---
+++ TP1.md
@@ -0,0 +1,10 @@
+# Creation d'un CRUD en .NET
+
+## Consignes
+
+Pour les entites Teacher et Student, vous devez creer un CRUD avec des données en dur:
+
+- Create: Ajouter un nouvel élément
+- Read: Lire un élément
+- Update: Modifier un élément
+- Delete: Supprimer un élément
